cdf525ae324e77b15eaf2361d3a29a90e8c7466a
Leonti Bielski <lbielski@seek.com.au>
2019-05-30 12:37:18 +1000

    Read from csv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+extern crate csv;
+#[macro_use]
+extern crate serde_derive;
+use csv::Error;
+
+#[derive(Debug, Deserialize)]
+struct Record {
+    time: usize,
+    delta: usize,
+    x: f32,
+    y: f32,
+    z: f32,
+    gyro_x: f32,
+    gyro_y: f32,
+    gyro_z: f32,
+}
+
+fn main() -> Result<(), Error> {
+
+    let mut reader = csv::Reader::from_path("axl_gyro.csv")?;
+
+    for record in reader.deserialize::<Record>() {
+        let record: Record = record?;
+        println!("{:?}", record);
+    }
+
+    Ok(())
 }
